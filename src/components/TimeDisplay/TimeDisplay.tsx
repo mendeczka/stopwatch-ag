5e81851e048f66e324cec783dbbb3df1e5a3f9fd
mendeczka <mendeczka@o2.pl>
2024-05-15 13:44:15 +0200

    Added minutes, second and milisec

--- src/components/TimeDisplay/TimeDisplay.tsx
+++ src/components/TimeDisplay/TimeDisplay.tsx
@@ -1,10 +1,15 @@
 import {TimeDisplayProps} from "../../types/time.ts";
 
 export const TimeDisplay = ({label, time}: TimeDisplayProps) => {
+
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time% 60000) / 1000);
+    const miliseconds = Math.floor(time % 1000) / 100;
+
     return (
         <div>
             <h2>{label}</h2>
-            <div>{time}</div>
+            <div>{minutes} : {seconds} : {miliseconds}</div>
         </div>
     );
 };
